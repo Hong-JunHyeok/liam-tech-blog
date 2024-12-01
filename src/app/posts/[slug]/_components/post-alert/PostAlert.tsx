f4dc084baa4a702bd02b71634e70d9aa1b0835bc
liam-gallagher-clap <liam@dwhale.kr>
2024-12-01 23:04:13 +0900

    [Hotfix] alert 깃허브 링크가 먹지않는 문제를 수정한다.

--- src/app/posts/[slug]/_components/post-alert/PostAlert.tsx
+++ src/app/posts/[slug]/_components/post-alert/PostAlert.tsx
@@ -5,14 +5,14 @@ type Props = {
 };
 
 export default function PostAlert({ title }: Props) {
-  const getPostToGithubLink = `${title}`;
+  const GITHUB_FILE_LINK = `https://github.com/Hong-JunHyeok/liam-tech-blog/blob/main/_posts/${title}.md`;
 
   return (
     <div className="border-b bg-accent-1 border-accent-2 fixed top-0 w-full">
       <div className="container mx-auto px-5">
         <div className="py-2 text-center text-sm ">
           지금 보시는 글은 100% 완벽하지 않습니다. 수정사항은{" "}
-          <Link className="text-success underline" href={getPostToGithubLink}>
+          <Link className="text-success underline" href={GITHUB_FILE_LINK}>
             여기
           </Link>
           로 요청주세요.
